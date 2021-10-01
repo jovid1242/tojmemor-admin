c1e884bd50a2f5fe8e2764c39bd31ae7996c648d
jovid1242 <jovid1242@gmail.com>
2021-10-01 16:50:49 +0500

    fiuxx

--- src/http.js
+++ src/http.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const http = axios.create({
-    baseURL: 'http://localhost:9001/api/',
+    baseURL: 'http://tojbackend.web.bobo.tj/api/',
     headers: {
         'Content-Type': 'application/json',
         Accept: 'application/json',
